e1fed3a821e5150de5a664db17dde4f47b652e00
karel <kmarreroe86@gmail.com>
2018-01-31 17:07:48 -0500

    Animations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,31 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-import {trigger, style, transition, animate, keyframes, query, stagger} from '@angular/animations';
+import { trigger, query, animate, keyframes } from '@angular/animations';
+import {
+  trigger,
+  style,
+  transition,
+  animate,
+  keyframes,
+  query,
+  stagger
+} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   animations: [
-    
+    trigger('goals', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), { optional: true }),
+        query(':enter', stagger('300ms', [
+            animate('.6s ease-in', keyframes([
+                style({ opacity: 0, transform: 'translateY(-75%)', offset: 0 }),
+                style({ opacity: 0, transform: 'translateY(35px)', offset: 0.3 }),
+                style({ opacity: 0, transform: 'translateY(0)', offset: 1 })
+              ])) ]), { optional: true })
+      ])
+    ])
   ]
 })
 export class HomeComponent implements OnInit {
-
   itemCount: number;
-  btnText:string = "Add an item";
-  goalText:string = "My first life goal";
+  btnText = 'Add an item';
+  goalText = 'My first life goal';
   goals = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.itemCount = this.goals.length;
   }
 
-  addItem = function() {    
+  addItem = function() {
     this.goals.push(this.goalText);
-    this.goalText = "";
+    this.goalText = '';
     this.itemCount = this.goals.length;
-  }
-
+  };
 }
